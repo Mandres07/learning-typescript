24f559bdabb2f555d38a11fb8c26930bce6d19d2
Mario Morales <mario.morales@devsu.com>
2022-03-30 12:35:59 -0500

    structured folders

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+const userName = 'Mario';
+console.log(userName);
+// any comment
+
+const button = document.querySelector('button')! as HTMLButtonElement;
+button.addEventListener('click', () => {
+   alert('clicked');
+});
