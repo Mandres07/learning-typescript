d23423a12eabbc41d020485e213a26d39b7a9aad
Mario Morales <mario.morales@devsu.com>
2022-03-30 12:57:50 -0500

    updates on tsconfig

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,11 @@
 "use strict";
 const userName = 'Mario';
 console.log(userName);
+function clickHandler(message) {
+    console.log('Clicked: ' + message);
+}
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     alert('clicked');
 });
+button.addEventListener('click', clickHandler.bind(null, 'Thanks!'));
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,13 @@ const userName = 'Mario';
 console.log(userName);
 // any comment
 
+function clickHandler(message: string) {
+   console.log('Clicked: ' + message);
+}
+
 const button = document.querySelector('button')! as HTMLButtonElement;
 button.addEventListener('click', () => {
    alert('clicked');
 });
+
+button.addEventListener('click', clickHandler.bind(null, 'Thanks!'));
